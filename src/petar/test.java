eb3fe912d84ab4ae0d93e28852cacb864da8a3d1
pradosavljevic <pradosavljevic@student.tgm.ac.at>
2017-03-29 21:58:08 +0200

    2

--- src/petar/test.java
+++ src/petar/test.java
@@ -3,6 +3,8 @@ package petar;
 public class test {
 	public static void main(String[]args) {
 		System.out.println("Hallo petar!");
+		System.out.println("Hallo petar!");
+		System.out.println("Hallo petar!");
 	}
 
 }
